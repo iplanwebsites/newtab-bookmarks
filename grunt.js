9f16b13492714238666d129583024819ef55994e
Simon Boudrias <admin@simonboudrias.com>
2012-11-25 16:37:14 -0500

    fix grunt linting

--- grunt.js
+++ grunt.js
@@ -1,6 +1,10 @@
 // This is the main application configuration file.  It is a Grunt
 // configuration file, which you can learn more about here:
 // https://github.com/cowboy/grunt/blob/master/docs/configuring.md
+
+/*jshint strict:false */
+/*global module */
+
 module.exports = function( grunt ) {
 	
 	grunt.initConfig({
